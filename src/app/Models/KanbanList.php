59e89bf7ac46ab58e6b3126aa99738fc782f7cd6
ycaro.pierozzi <ycaro.pierozzi@ambrait.com>
2025-06-11 19:32:54 -0300

    generating models, migrations, factories and controllers default classes

---
+++ src/app/Models/KanbanList.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class KanbanList extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'board_id',
+        'title',
+        'position',
+    ];
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+
+}
