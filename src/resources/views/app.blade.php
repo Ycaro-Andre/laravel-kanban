4603bd377e2003887a9c59ecda1364db3491b5ef
ycaro.pierozzi <ycaro.pierozzi@ambrait.com>
2025-06-13 00:31:24 -0300

    correcting font awesome CDN load

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -12,7 +12,7 @@
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-            integrity="sha512-Ff2D8nLpiUqC51hTIr5jqfwECgZyd+k4rAlH3Mp4+7BoXiTfKhg8we5kmx5pY0ZvhMIphXZ8pED6Zi9aYld3CQ=="
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
